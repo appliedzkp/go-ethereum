374d293d519d2b92b0bc407fab86c758e259f655
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2022-02-03 06:40:56 +0000

    workflows: only build docker images for linux/{amd64,arm64}

--- .github/workflows/docker.sh
+++ .github/workflows/docker.sh
@@ -12,5 +12,5 @@ fi
 echo $image:$tag
 docker buildx create --name mybuilder --use || echo 'skip'
 docker buildx inspect --bootstrap
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 -t $image:$tag --push .
+docker buildx build --platform linux/amd64,linux/arm64 -t $image:$tag --push .
 docker buildx imagetools inspect $image:$tag
