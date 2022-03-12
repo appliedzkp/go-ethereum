73b68fdc12c991fe1159d576cd13a42bb2d878a9
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2022-03-12 11:01:30 +0000

    zkevm: wrongly excluded PrepareAccessList

--- core/state_transition.go
+++ core/state_transition.go
@@ -311,7 +311,7 @@ func (st *StateTransition) TransitionDb() (*ExecutionResult, error) {
 	}
 
 	// Set up the initial access list.
-	if rules := st.evm.ChainConfig().Rules(st.evm.Context.BlockNumber, st.evm.Context.Random != nil); rules.IsBerlin && !rules.IsZkEvm {
+	if rules := st.evm.ChainConfig().Rules(st.evm.Context.BlockNumber, st.evm.Context.Random != nil); rules.IsBerlin {
 		st.state.PrepareAccessList(msg.From(), msg.To(), vm.ActivePrecompiles(rules), msg.AccessList())
 	}
 	var (
